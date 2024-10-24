edbb22166c5582b1c02c027c0c1f0df006d67146
Sinan Balıbey <sinanbey23@hotmail.com>
2024-10-24 21:51:30 +0300

    Base

---
+++ src/test/java/com/FlashCardgai/FlashCard/FlashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlashCardgai.FlashCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
